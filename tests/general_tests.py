0cd33183242493681a78d763bb22afcbfc240f7b
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-12-08 23:59:52 -0400

    Finished the single battery tests and started working on cluster tests.

--- tests/general_tests.py
+++ tests/general_tests.py
@@ -1,6 +1,7 @@
 from battery import battery
 
-# Honestly I think this one battery test alone is pretty worthless
+# ----------------------- single battery tests -----------------------
+
 def test_1_battery():
   b = battery("test_battery", 1, 0, startSoc=100)
   possible_transfer = b.calc_possible_powerTransfer(2, "discharge")
@@ -23,3 +24,51 @@ def test_1_battery():
   assert cycle_deg == 4.6/1000/2
   calender_deg = b.calender_degredation(2)
   assert calender_deg == (0.5/100)/(365*24) * 2
+  # Now for charging
+  b = battery("test_battery", 1, 0, startSoc=60)
+  possible_transfer = b.calc_possible_powerTransfer(1, "charge")
+  # if I charge a battery with inverterMaxCharge=10
+  # for 1 hours I should get:
+  # power_transfer = 10 and new_currentEnergy = 40*0.6+10
+  assert possible_transfer[0]==10
+  assert possible_transfer[1]==34
+  # Now I can test the degredations again
+  cycle_deg = b.cycle_degredation(possible_transfer[0])
+  # now because I am discharging for quarter of a cycle
+  assert cycle_deg == 4.6/1000/4
+  calender_deg = b.calender_degredation(1)
+  assert calender_deg == (0.5/100)/(365*24) * 1
+  # and the edge cases like charging near full charge and
+  # discharging when almost out
+  b = battery("test_battery", 1, 0, startSoc=80)
+  possible_transfer = b.calc_possible_powerTransfer(1, "charge")
+  # considering maxEnergy is 85% of the capacity
+  assert possible_transfer[0]==40*0.05
+  assert possible_transfer[1]==40*0.85
+  # and min energy is 20%
+  b = battery("test_battery", 1, 0, startSoc=25)
+  possible_transfer = b.calc_possible_powerTransfer(1, "discharge")
+  assert possible_transfer[0]==-40*0.05
+  assert possible_transfer[1]==40*0.2
+
+# So far test one has estanlished that calc_possible_powerTransfer
+# and the degredation functions work
+# test 2 will check if it correctly changes the battery in accordance
+def test_2_battery():
+  b = battery("test_battery", 1, 0, startSoc=100)
+  result_1 = b.transfer_energy(3, "discharge")
+  assert result_1["energy_transfer"]==30
+  assert result_1["degredation"]==4.6/1000*3/4+(0.5/100)/(365*24) * 3
+  assert b.capacity == 40-result_1["degredation"]
+  assert b.currentEnergy == 40-30
+  num_cycles = 20/b.capacity
+  result_2 = b.transfer_energy(2, "charge")
+  assert result_2["energy_transfer"]==20
+  assert result_2["degredation"]==4.6/1000*num_cycles+(0.5/100)/(365*24) * 2
+  assert b.capacity == 40-result_1["degredation"]-result_2["degredation"]
+  assert b.currentEnergy == 40-30+20
+
+# ----------------------- cluster tests -----------------------
+
+def test_1_cluster():
+  pass
