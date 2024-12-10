fcca374f0ca7ed257df94def2e10b22da0fdfd0d
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-12-09 21:27:25 -0400

    I think I am dine with these fucking tests

--- tests/simulation_test.py
+++ tests/simulation_test.py
@@ -18,8 +18,6 @@ def make_cluster(n, SOCs):
 # in these tests but since I already did that in the general tests I wont do that
 def test_simulation():
   c = make_cluster(16, 100)
-  # A summer day consists of
-  # 12 OFF_PEAK, 6 MID_PEAK and 6 ON_PEAK hours
   assert c.total_energy==16*40
   assert c.total_capacity==16*40
   # Off peak when at 100%
@@ -48,4 +46,39 @@ def test_simulation():
   assert c.total_energy==16*40*0.3
   c.share_load(d["decision"], d["amount"], 1, OFF_PEAK)
   assert c.total_energy>=16*40*0.3 + 90
+  assert c.total_capacity<16*40
+  # Mid peak at 70% - discharge
+  c = make_cluster(16, 70)
+  d = make_decision(MID_PEAK, c)
+  assert c.total_energy==16*40*0.7
+  c.share_load(d["decision"], d["amount"], 1, OFF_PEAK)
+  assert c.total_energy<=16*40*0.7 - 40
+  assert c.total_capacity<16*40
+  # Mid peak at 25% -> shouldnt be any chages
+  c = make_cluster(16, 25)
+  d = make_decision(MID_PEAK, c)
+  assert c.total_energy==16*40*0.25
+  c.share_load(d["decision"], d["amount"], 1, OFF_PEAK)
+  assert c.total_energy==16*40*0.25
+  assert c.total_capacity==16*40
+  # On peak at 85% - discharge
+  c = make_cluster(16, 85)
+  d = make_decision(ON_PEAK, c)
+  assert c.total_energy==16*40*0.85
+  c.share_load(d["decision"], d["amount"], 1, OFF_PEAK)
+  assert c.total_energy<=16*40*0.85 - 100
+  assert c.total_capacity<16*40
+  # On peak at 75% - discharge
+  c = make_cluster(16, 75)
+  d = make_decision(ON_PEAK, c)
+  assert c.total_energy==16*40*0.75
+  c.share_load(d["decision"], d["amount"], 1, OFF_PEAK)
+  assert c.total_energy<=16*40*0.75 - 90
+  assert c.total_capacity<16*40
+  # On peak at 60% - discharge
+  c = make_cluster(16, 60)
+  d = make_decision(ON_PEAK, c)
+  assert c.total_energy==16*40*0.60
+  c.share_load(d["decision"], d["amount"], 1, OFF_PEAK)
+  assert c.total_energy<=16*40*0.60 - 80
   assert c.total_capacity<16*40
